973a47cb349124617490e36e7252c27ceecbfbe5
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-11-13 12:49:48 +0000

    Task 4 from 1st assignment done

--- assignment-section2/app.js
+++ assignment-section2/app.js
@@ -2,25 +2,30 @@ const app = Vue.createApp({
     data() {
         return {
             myName: 'Cris 😊',
-            myAge: 30 + ' ' + 'years' ,
+            myAge: 30 + ' years' ,
+            falseMessage: 'No, a bit higher',
+            trueMessage: 'Almost there'
         };
     },
+
     methods: {
         addAge() {
             let currentDate = new Date();
             let birthDate = new Date("1989/11/06");
             let difference = currentDate - birthDate;
-            let age = Math.floor(difference/31557600000 + (5)); // adds 5 more years to my age
+            let age = Math.floor(difference/31557600000 + (5)) + ' years'; // adds 5 more years to my age
             return age
-          }
+        },
+
+        myRandomNumber() {
+            const favoriteNumber = Math.random();
+            if (favoriteNumber <0.5) {
+                return this.falseMessage;
+            } else {
+                return this.trueMessage;
+            }
         }
+    }
 });
 
 app.mount('#assignment');
-
-// addAge(dob) { 
-//     var diff_ms = Date.now() - dob.getTime();
-//     var age_dt = new Date(diff_ms); 
-  
-//     return Math.abs(age_dt.getUTCFullYear() - 1989 + (5));
-// }
