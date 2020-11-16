4410b52238dc1eb720c487adef9bf7928815aea3
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-11-16 18:50:10 +0000

    Exercise lesson 21

---
+++ class 21/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        counter: 0,
+      };
+    },
+    methods: {
+        // this method above doesn't return anything because it doesn't has to, because the click listener doesnt want a return value
+        addCounter() {
+            this.counter++;
+            // or this.counter = this.conter + 1, it's the same thing
+        },
+
+        reduceCounter() {
+            this.conter--;
+        }
+    }
+  });
+  
+  app.mount('#events');
