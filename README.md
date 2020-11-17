d4c19c9469187c70b82c71a5a979cd65aa4e4eab
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-11-17 16:13:22 +0000

    Exercise Lesson 23: event default

--- README.md
+++ README.md
@@ -42,4 +42,5 @@ Lesson 22:
 Lesson 23:
     * using the native `event` object;
     * `event` as an argument;
+    * `input` event, a default DOM event available on input elements (nothing Vue specific) also listened in vanilla JS;
     * update of a property (name, in this case) whenever it is typed in the `input` field which will be outputed in our paragraph.
--- class 23/app.js
+++ class 23/app.js
@@ -2,9 +2,15 @@ const app = Vue.createApp({
     data() {
       return {
         counter: 0,
+        name: ''
       };
     },
     methods: {
+        setName(event) {
+          // event.target give us access to the target, the element on which the event occurred
+          this.name = event.target.value; // it will be the value the user enters
+        },
+
         addCounter(number) {
           this.counter = this.counter + number;
         },
