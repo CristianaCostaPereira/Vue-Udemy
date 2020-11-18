2e5d82f4b7f17519d7528e0b9146036b04e3dbf5
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-11-18 20:26:29 +0000

    Assignment 3, section 2: solution and code review

--- assignments/assignments_section2/assignment_3/app.js
+++ assignments/assignments_section2/assignment_3/app.js
@@ -7,6 +7,30 @@ const app = Vue.createApp({
         }
     },
 
+    methods: {
+        addNumber(number) {
+            this.counter = this.counter + number;
+        },
+    },
+
+    // We just don't want to output the number, we want to output a result which is either some text or the number
+    // Now we have the message computed property which depends on our counter property and Vue detectes this dependency and will only re-calculate and re-evaluate message when the counter changes
+    computed: {
+        message() {
+            if (this.counter < 37) {
+                return this.messageOne;
+            } else if (this.counter === 37) {
+                return this.counter;
+            } else {
+                return this.messageTwo;
+            }
+        }
+    },
+
+    // Watch for changes in the message computed properterty
+    // Whenever message CHANGES, whenever has a new value, we want to trigger a watcher
+    // watcher will only executes if message switch for message one, to a number or from a number to message two, or from message one to message two
+    // So only if the value ield by message really changed, only in this case the watcher executes
     watch: {
         counter(value) {
             if (value <= 37) {
@@ -17,22 +41,6 @@ const app = Vue.createApp({
             }
         }
     },
-
-    computed: {
-        message() {
-            if (this.counter <= 37) {
-                return this.messageOne;
-            } else {
-                return this.messageTwo;
-            }
-        }
-    },
-
-    methods: {
-        addNumber(number) {
-            this.counter = this.counter + number;
-        },
-    }
 });
 
 app.mount('#assignment');
