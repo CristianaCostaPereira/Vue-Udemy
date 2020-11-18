f053b56cf66de5979d6103de44141526181f531c
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-11-18 19:26:35 +0000

    Assignment 3, section 2: task 1.2 e 1.3 completed

--- assignments/assignments_section2/assignment_3/app.js
+++ assignments/assignments_section2/assignment_3/app.js
@@ -1,10 +1,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter: 0
+            counter: 0,
+            messageOne: 'Not there yet',
+            messageTwo: 'Too much!'
         }
     },
-    
+
+    computed: {
+        message() {
+            if (this.counter <= 37) {
+                return this.messageOne;
+            } else {
+                return this.messageTwo;
+            }
+        }
+    },
+
     methods: {
         addNumber(number) {
             this.counter = this.counter + number;
