673d9f176c243e3655780d4e2edcf8f52ee9ed5e
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-11-18 13:13:48 +0000

    Lesson 27: some problems we may have using methods in certain situations

--- README.md
+++ README.md
@@ -60,4 +60,9 @@ Assignment 2 section 2:
 Lesson 26:
     * `v-model`;
     * data binding + event binding = two-way binding;
-    * two-way binding: because we are communicating in two ways; we are listening to an event coming out of the input element to the input event and, at the same time, we are writing the value back to the input element through its value attribute;
+    * two-way binding: because we are communicating in two ways; we are listening to an event coming out of the input element to the input event and, at the same time, we are writing the value back to the input element through its value attribute.
+
+Lesson 27:
+    * methods used for data binding: how it works;
+    * 'advanced' reactivity;
+    * a better alternative to 'methods' in certain cases
---
+++ class 27/app.js
@@ -0,0 +1,40 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: ''
+    };
+  },
+    
+  methods: {
+    // The problem with his is when we press the counter button, Vue tries to find out where, on the HTML, it needs to update the rendered page
+    outputFullName() {
+      console.log('Running')
+      // Since we are using a method we can add an if statement
+      if (this.name === '') {
+        return ''; // so we only add 'Pereira' if the name is NOT empty
+      }
+      // Should return something, because we are not going to use it to bind it to an event but, instead call it in the HTML
+      return this.name + ' ' + 'Pereira';
+    },
+
+    setName(event) {
+      this.name = event.target.value;
+    },
+  
+    add(num) {
+      this.counter = this.counter + num;
+    },
+  
+    reduce(num) {
+      this.counter = this.counter - num;
+    },
+  
+    resetInput() {
+      this.name = '';
+    }
+  }
+});
+  
+app.mount('#events');
+  
