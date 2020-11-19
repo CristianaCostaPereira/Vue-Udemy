7c322f5aa184778c4ad41a43fa3b21c87c48611d
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-11-19 14:20:23 +0000

    Assignment 4, section 2: task two solution

--- assignments/assignments_section2/assignment_4/app.js
+++ assignments/assignments_section2/assignment_4/app.js
@@ -3,8 +3,28 @@ const app = Vue.createApp({
         return {
             // Store the user input
             userInput: '',
+            visibleParagraph: true,
         }
     },
+
+    // Because we added more classes
+    computed: {
+        paragraphClasses() {
+            return {
+                user1: this.userInput === 'user1',
+                user2: this.userInput === 'user2',
+                visible: this.visibleParagraph,
+                hidden: !this.visibleParagraph
+            };
+        },
+    },
+
+    // We want to change this property on every click
+    methods: {
+        toggleVisible() {
+            this.visibleParagraph = !this.visibleParagraph
+        }
+    }
 });
 
 app.mount('#assignment');
