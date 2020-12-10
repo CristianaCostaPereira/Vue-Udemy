26c2956b1fcad981481b2f0f56b32458dc5e994f
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-12-10 15:50:01 +0000

    CSS

--- vue-first-app/src/App.vue
+++ vue-first-app/src/App.vue
@@ -91,7 +91,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -123,5 +124,21 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
 }
 </style>
