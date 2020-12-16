b84dc172218b7f8e9c827e57becb6b2bd8ba0184
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-12-16 11:18:05 +0000

    Lesson 114: starting dynamic components

--- vue-section9-app/src/App.vue
+++ vue-section9-app/src/App.vue
@@ -1,38 +1,23 @@
 <template>
   <div>
     <the-header></the-header>
-    <!-- <TheHeader /> -->
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
+    <active-goals></active-goals>
+    <manage-goals></manage-goals>
     
-    <course-goals>
-      <!-- Our scoped slot: allows to pass data from inside the component where we defined a slot to the component where we passed the markup for the slot and we do this by going to the component and add a prop to the slot -->
-      <template #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-
-        <p>{{ slotProps.anotherProp }}</p>
-      </template>
-    </course-goals>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
   // components only usable here, in this file
   components: {
-    TheHeader, // or 'the-header': TheHeader
-    BadgeList,
-    UserInfo,
-    CourseGoals
+    TheHeader,
+    ActiveGoals,
+    ManageGoals
   },
 
   data() {
