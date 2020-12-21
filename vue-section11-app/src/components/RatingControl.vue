95308ee34c0bd3c209b3b24988ceb253d54db190
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-12-21 16:13:14 +0000

    Lesson 143: custom control components built

---
+++ vue-section11-app/src/components/RatingControl.vue
@@ -0,0 +1,56 @@
+<template>
+  <ul>
+    <li :class="{active: activeOption === 'poor'}"><button type="button" @click="activate('poor')">Poor</button></li>
+    <li :class="{active: activeOption === 'average'}"><button type="button" @click="activate('average')">Average</button></li>
+    <li :class="{active: activeOption === 'great'}"><button type="button" @click="activate('great')">Great</button></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeOption: null
+    };
+  },
+
+  methods: {
+    activate(option) {
+      this.activeOption = option;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.active {
+  border-color: #139bb3;
+}
+
+.active button {
+  color: #139bb3;
+}
+
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
--- vue-section11-app/src/components/TheForm.vue
+++ vue-section11-app/src/components/TheForm.vue
@@ -77,6 +77,10 @@
     </div>
 
     <div class="form-control">
+      <rating-control></rating-control>
+    </div>
+
+    <div class="form-control">
       <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm" />
       <label for="confirm-terms">Agree to terms of use?</label>
     </div>
@@ -88,7 +92,13 @@
 </template>
 
 <script>
+import RatingControl from './RatingControl.vue';
+
 export default {
+components: {
+  RatingControl
+},
+
   data() {
     return {
       userName: '',
