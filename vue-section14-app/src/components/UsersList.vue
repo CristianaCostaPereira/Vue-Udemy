71f4e3726686f25f3b4ca428359963a87b14f054
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-01-22 13:50:00 +0000

    Lesson 202: animating lists with transition-group component

--- vue-section14-app/src/components/UsersList.vue
+++ vue-section14-app/src/components/UsersList.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <ul> 
-     <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
-    </ul>
+    <!-- Capable of animate individual lists items, but also multiple elements at the same time, making it great for lists -->
+    <!-- Will render an element to the DOM and we can control which element by adding the tag prop -->
+    <transition-group tag="ul" name="user-list">
+      <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
+    </transition-group>
 
     <div>
       <input type="text" ref="userNameInput"/>
       <button @click="addUser">Add Users 🖋️</button>
     </div>
-
   </div>
 </template>
 
@@ -52,5 +53,29 @@ li {
   border: 1px solid #ccc;
   padding: 1rem;
   text-align: center;
+}
+
+.user-list-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.user-list-enter-active {
+  transition: all 1s ease-out; 
+}
+
+.user-list-enter-to,
+.user-list-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.user-list-leave-active {
+  transition: all 1s ease-in; 
+}
+
+.user-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
