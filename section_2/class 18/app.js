f3873c69fb036fa0ff8af1491322b018895f65f2
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2022-04-06 19:43:22 +0100

    Code Review: Indentation

--- section_2/class 18/app.js
+++ section_2/class 18/app.js
@@ -1,23 +1,25 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            courseGoalA: 'Learn Vue by completing the course😄 !',
+  data() {
+    return {
+      courseGoalA: 'Learn Vue by completing the course 😄 !',
 
-            // Below we are assuming courseGoalB is HTML code
-            courseGoalB: '<h2>Master Vue and build amazing apps 🧐 !</h2>',
-            vueLink: 'https://vuejs.org/'
-        };
-    },
-    methods: {
-        outputGoal() {
-            const randomNumber = Math.random();
-            if (randomNumber <0.5) {
-                return this.courseGoalA;
-            } else {
-                return this.courseGoalB;
-            }
-        }
+      // Bellow we are assuming courseGoalB is HTML code
+      courseGoalB: '<h2>Master Vue and build amazing apps 🧐 !</h2>',
+      vueLink: 'https://vuejs.org/'
+    };
+  },
+
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+
+      if (randomNumber <0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
     }
+  }
 });
 
 app.mount('#user-goal');
