fd7da5096247a43077c552dfbb780e91ef830caa
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2022-04-08 19:17:59 +0100

    Code Review: Indentation

--- assignments/assignments_section2/assignment_2/app.js
+++ assignments/assignments_section2/assignment_2/app.js
@@ -1,24 +1,24 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            message: '',
-            addInput: ''
-        };
+  data() {
+    return {
+      message: '',
+      addInput: ''
+    };
+  },
+
+  methods: {
+    showAlert() {
+      alert('On my way to master Vue 😄 !');
     },
 
-    methods: {
-        showAlert() {
-            alert('On my way to master Vue 😄 !');
-        },
+    setInput(event) {
+      this.message = event.target.value; // What the user enters
+    },
 
-        setInput(event) {
-            this.message = event.target.value; // What the user enters
-        },
-
-        confirmInput() {
-            this.addInput = this.message // Sets it equal to the internally stored message
-        }
+    confirmInput() {
+      this.addInput = this.message // Sets it equal to the internally stored message
     }
+  }
 });
 
 app.mount('#assignment');
