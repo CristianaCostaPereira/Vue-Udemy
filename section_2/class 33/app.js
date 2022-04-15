a3db6c9637d7354e31f6d69457fd4ecff3da9a4f
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2022-04-15 10:47:07 +0100

    Code Review: Indentation

--- section_2/class 33/app.js
+++ section_2/class 33/app.js
@@ -1,24 +1,24 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            boxASelected: false,
-            boxBSelected: false,
-            boxCSelected: false,
-        }
-    },
-    
-    methods: {
-        // So users can toggle the div's and not just set it as activem but also as inactive
-        boxSelected(box) {
-            if (box === 'A') {
-                this.boxASelected = !this.boxASelected; // if box A was selected, was true, it is now false because we added the (!) 
-            } else if (box === 'B') {
-                this.boxBSelected = !this.boxBSelected;
-            } else {
-                this.boxCSelected = !this.boxCSelected;
-            }
-        }
+  data() {
+    return {
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
     }
+  },
+    
+  methods: {
+    // So users can toggle the div's and not just set it as activem but also as inactive
+    boxSelected(box) {
+      if (box === 'A') {
+        this.boxASelected = !this.boxASelected; // if box A was selected, was true, it is now false because we added the (!) 
+      } else if (box === 'B') {
+        this.boxBSelected = !this.boxBSelected;
+      } else {
+        this.boxCSelected = !this.boxCSelected;
+      }
+    }
+  }
 });
 
 app.mount('#styling');
