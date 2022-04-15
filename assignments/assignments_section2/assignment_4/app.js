2aced75aa71c9b8ff01a987a989ea823ee6fa9c6
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2022-04-15 14:18:02 +0100

    Code Review: Comment logic corrected  and indentation

--- assignments/assignments_section2/assignment_4/app.js
+++ assignments/assignments_section2/assignment_4/app.js
@@ -1,30 +1,30 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            userInput: '', // Store the user input, task 1
-            paragraphVisibility: true, // to show or hide the paragraph, task 2
-            inlineBackgroundColor: '' // task 3
-        }
-    },
-
-    // Because we added more classes
-    computed: {
-        paragraphClasses() {
-            return {
-                user1: this.userInput === 'user1',
-                user2: this.userInput === 'user2',
-                visible: this.paragraphVisibility,
-                hidden: !this.paragraphVisibility
-            };
-        },
-    },
-
-    // We want to change this property on every click
-    methods: {
-        toggleVisibility() {
-            this.paragraphVisibility = !this.paragraphVisibility
-        }
+  data() {
+    return {
+      userInput: '', // Store the user input, task 1
+      paragraphVisibility: true, // to show or hide the paragraph, task 2
+      inlineBackgroundColor: '' // task 3
     }
+  },
+
+  // Because we added more classes
+  computed: {
+    paragraphClasses() {
+      return {
+        user1: this.userInput === 'user1',
+        user2: this.userInput === 'user2',
+        visible: this.paragraphVisibility,
+        hidden: !this.paragraphVisibility
+      };
+    },
+  },
+
+  // We want to change this property on every click
+  methods: {
+    toggleVisibility() {
+      this.paragraphVisibility = !this.paragraphVisibility
+    }
+  }
 });
 
 app.mount('#assignment');
