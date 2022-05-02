cf3dcbbf99d3beffe5d5cc66a3d7a607e10be241
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2022-05-02 21:32:10 +0100

    Code Review: Indentation

--- assignments/assignments_section3/assignment_5/app.js
+++ assignments/assignments_section3/assignment_5/app.js
@@ -1,27 +1,27 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            tasks: [],
-            enteredTask: '',
-            showList: true,
-        };
-    },
+  data() {
+    return {
+      tasks: [],
+      enteredTask: '',
+      showList: true,
+    };
+  },
 
-    computed: {
-        buttonText() {
-            return this.showList ? 'Hide List' : 'Show List';
-        }
-    },
-
-    methods: {
-        addTask() {
-            this.tasks.push(this.enteredTask);
-        },
-
-        toggleList() {
-            this.showList = !this.showList;
-        },
+  computed: {
+    buttonText() {
+      return this.showList ? 'Hide List' : 'Show List';
     }
+  },
+
+  methods: {
+    addTask() {
+      this.tasks.push(this.enteredTask);
+    },
+
+    toggleList() {
+      this.showList = !this.showList;
+    },
+  }
 });
 
 app.mount('#assignment');
