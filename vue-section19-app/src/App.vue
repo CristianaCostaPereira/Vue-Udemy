1ae3f622121abd26cdfb8a3c488b6bc92e6137e6
cristiana.costa.ext <cristiana.costa.ext@spms.min-saude.pt>
2022-08-24 18:13:35 +0100

    multiple dependencies syntax that trigger the watcher function.

--- vue-section19-app/src/App.vue
+++ vue-section19-app/src/App.vue
@@ -25,7 +25,7 @@
 <script setup>
 import { ref, reactive, computed, watch } from 'vue';
 
-// const userName = ref('Cristiana')
+// const name = ref('Cristiana')
 // const age = ref(31)
 // const firstName = ref('')
 // const lastName = ref('')
@@ -48,6 +48,15 @@ watch(age, (newValue, oldValue) => {
 const userName = computed(() => {
   return user.firstName + ' ' + user.lastName
 })
+
+// multiple dependencies that trigger the watcher function
+// watch([age, userName], (newValues, oldValues) => {
+//   console.log('Old age: ' + oldValues[0])
+//   console.log('New age: ' + newValues[0])
+
+//   console.log('Old name: ' + oldValues[1])
+//   console.log('New name: ' + newValues[1])
+// })
 
 function setNewAge () {
   age.value = 33
