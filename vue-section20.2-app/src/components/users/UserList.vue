65ed7b03d6d3b30c89b73bf431150f9abf37477b
cristiana.costa.ext <cristiana.costa.ext@spms.min-saude.pt>
2022-09-20 12:48:42 +0100

    sort.js file; useSort hook in UserList.

--- vue-section20.2-app/src/components/users/UserList.vue
+++ vue-section20.2-app/src/components/users/UserList.vue
@@ -36,10 +36,11 @@
 </template>
 
 <script>
-import { ref, computed, toRefs } from 'vue';
+import { toRefs } from 'vue'
 
-import UserItem from './UserItem.vue';
+import UserItem from './UserItem.vue'
 import useSearch from '../../hooks/search'
+import useSort from '../../hooks/sort'
 
 export default {
   components: {
@@ -54,31 +55,10 @@ export default {
     // Ensure the useSearch always receive a ref as its items
     const { users } = toRefs(props)
 
+    // Custom hooks
     const { enteredSearchTerm, availableItems, updateSearch } = useSearch(users, 'fullName')
 
-    const sorting = ref(null)
-
-    const displayedUsers = computed(function () {
-      if (!sorting.value) {
-        return availableItems.value
-      }
-
-      return availableItems.value.slice().sort((u1, u2) => {
-        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
-          return 1
-        } else if (sorting.value === 'asc') {
-          return -1
-        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
-          return -1
-        } else {
-          return 1
-        }
-      })
-    })
-
-    function sort(mode) {
-      sorting.value = mode
-    }
+    const { sorting, displayedUsers, sort } = useSort(availableItems, 'fullName')
 
     return {
       enteredSearchTerm,
